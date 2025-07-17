5e1173864af08eeae2d36392d0cb9e5482cb3d23
Rohan Asawale <asawalerohan234@gmail.com>
2025-07-17 19:55:49 +0530

    240. Search a 2D Matrix II

---
+++ Medium/Solution.java
@@ -0,0 +1,29 @@
+package Medium;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0, col = m-1;
+
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {
+            {1,4,7,11,15},
+            {2,5,8,12,19},
+            {3,6,9,16,22},
+            {10,13,14,17,24},
+            {18,21,23,26,30}
+        };
+        int target = 5;
+        Solution sol = new Solution();
+        boolean ans = sol.searchMatrix(nums, target);
+    }
+}
